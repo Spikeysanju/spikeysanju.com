51fd82cadbd08399563f4273b59baebdd8d3d9c0
Spikey Sanju <spikeysanju98@gmail.com>
2023-02-17 07:02:53 +0530

    use relative import

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -2,10 +2,12 @@ import type { PageLoad } from './$types';
 
 export const load = (async ({ params }) => {
 	console.log('blog params', params.slug);
-	
-	const blog = await import(`/src/lib/data/blogs/${params.slug}.md?import`);
-	const { title, date, author, image, categories } = blog.metadata;
-	const content = blog.default;
+
+	// const blog = await import(`/src/lib/data/blogs/${params.slug}.md`);
+	const post = await import(`../../lib/data/books/${params.slug}.md`);
+
+	const { title, date, author, image, categories } = post.metadata;
+	const content = post.default;
 
 	return {
 		title: title,
@@ -14,6 +16,6 @@ export const load = (async ({ params }) => {
 		author: author,
 		slug: params.slug,
 		image: image,
-		categories: categories,
+		categories: categories
 	};
 }) satisfies PageLoad;
