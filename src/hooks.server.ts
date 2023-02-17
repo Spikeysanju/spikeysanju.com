784757621b850d86e2eba079f6a3c89e0df78e4d
Spikey Sanju <spikeysanju98@gmail.com>
2023-02-17 14:08:21 +0530

    remove trusthost

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,7 +8,6 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle = SvelteKitAuth({
 	adapter: PrismaAdapter(prisma) as Adapter,
-	trustHost: true,
 	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })] as any,
 	session: {
 		// Choose how you want to save the user session.
