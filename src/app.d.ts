b806418f9813cc40d7ff9a74285b0105cafc768e
Spikey Sanju <spikeysanju98@gmail.com>
2023-02-18 12:52:20 +0530

    add authkit types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,13 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
+/// <reference types="@auth/sveltekit" />
+
 declare namespace App {
-	// interface Error {}
+	// interface Error {
+	// 	status: number;
+	// 	message: string;
+	// }
 	// interface Locals {}
 	// interface PageData {}
 	// interface Platform {}
