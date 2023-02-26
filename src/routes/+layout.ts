20254051c0b127830dde7ce643e225dfbdc0c50d
Spikey Sanju <spikeysanju98@gmail.com>
2023-02-26 19:35:22 +0530

    Update config to use Node.js 16.x and set regions to US East 1, disabling split

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,7 @@
 import type { Config } from '@sveltejs/adapter-vercel';
 
 export const config: Config = {
-	runtime: 'nodejs16.x'
+	runtime: 'nodejs16.x',
+	regions: ['us-east-1'],
+	split: false,
 };
