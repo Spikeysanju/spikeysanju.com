ba52b8d9ba694508e8cdccd2269434aa88d8de3a
Spikey Sanju <spikeysanju98@gmail.com>
2023-02-26 19:45:32 +0530

    Remove '@sveltejs/adapter-vercel' and update svelte.config.js to use '@sveltejs/adapter-auto'

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 import rehypeSlug from 'rehype-slug';
@@ -7,21 +7,7 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// default options are shown
-		adapter: vercel({
-			// if true, will deploy the app using edge functions
-			// (https://vercel.com/docs/concepts/functions/edge-functions)
-			// rather than serverless functions
-			edge: false,
-
-			// an array of dependencies that esbuild should treat
-			// as external when bundling functions
-			external: [],
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app
-			split: false
-		})
+		adapter: adapter()
 	},
 	preprocess: [
 		{
