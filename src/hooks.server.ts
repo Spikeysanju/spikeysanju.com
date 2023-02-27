4c7288038817a2befa820e14c40d8902d0e5c6f1
Spikey Sanju <spikeysanju98@gmail.com>
2023-02-27 13:03:13 +0530

    Update `handle` to use `any` as provider type and set `debug` to `true`

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -14,7 +14,7 @@ export const handle = SvelteKitAuth({
 	providers: [
 		GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }),
 		Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })
-	],
+	] as any,
 	session: {
 		// Choose how you want to save the user session.
 		// The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
@@ -24,13 +24,13 @@ export const handle = SvelteKitAuth({
 		// which is used to look up the session in the database.
 		strategy: 'database',
 
-		// Seconds - How long until an idle session expires and is no longer valid.
-		maxAge: 30 * 24 * 60 * 60, // 30 days
+		// // Seconds - How long until an idle session expires and is no longer valid.
+		// maxAge: 30 * 24 * 60 * 60, // 30 days
 
-		// Seconds - Throttle how frequently to write to database to extend a session.
-		// Use it to limit write operations. Set to 0 to always update the database.
-		// Note: This option is ignored if using JSON Web Tokens
-		updateAge: 24 * 60 * 60, // 24 hours
+		// // Seconds - Throttle how frequently to write to database to extend a session.
+		// // Use it to limit write operations. Set to 0 to always update the database.
+		// // Note: This option is ignored if using JSON Web Tokens
+		// updateAge: 24 * 60 * 60, // 24 hours
 
 		// The session token is usually either a random UUID or string, however if you
 		// need a more customized session token string, you can define your own generate function.
@@ -38,7 +38,7 @@ export const handle = SvelteKitAuth({
 			return crypto.randomUUID();
 		}
 	},
-	debug: process.env.NODE_ENV === 'development' ? true : false,
+	debug: true,
 	events: {
 		signIn: async (message) => {
 			console.log('✅ Successfully signed in', message);
