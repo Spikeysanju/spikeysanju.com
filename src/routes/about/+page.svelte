d62652627850ad6cd1c6549f2e0e05179659ce80
Spikey Sanju <spikeysanju98@gmail.com>
2023-03-04 08:47:43 +0530

    update content on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,9 @@
 </script>
 
 <svelte:head>
-	<title>{data.title} | Spikey Sanju</title>
+	<title
+		>Hi, I'm Spikey Sanju, Designer & Engineer, Founder of THISUX Design Studio | Spikey Sanju</title
+	>
 	<meta name="description" content={data.description} />
 
 	<!-- canonical -->
@@ -20,10 +22,14 @@
 	<h1
 		class="text-3xl leading-tight tracking-tight sm:text-4xl sm:leading-snug lg:text-4xl lg:leading-tight"
 	>
+		Meet Spikey Sanju: Designer, Engineer, and ADHD-Inspired Innovator!
+	</h1>
+
+	<p>
 		When people ask me what I do, I tell them I'm a designer and an engineer. Then they give me that
 		confused look, and I tell them, "Think of me as a visual mechanic - I fix broken interfaces and
 		give them a fresh coat of pixels." My passion for design and engineering is like a marriage - we
 		may argue at times, but we always come to a happy resolution. As the founder of my own UX design
 		studio, I specialize in taking a user's experience from "meh" to "heck yeah!
-	</h1>
+	</p>
 </section>
