cb9016ed5bc26d171bed328eb97b6cf06dd20667
Spikey Sanju <spikeysanju98@gmail.com>
2023-04-01 05:57:19 +0530

    remove unused code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,69 +64,67 @@
 	</div>
 {/if}
 
-{#key data.currentRoute}
-	<main
-		class="mx-auto bg-white px-3 font-satoshi text-black dark:bg-deeppurple dark:text-white"
-		data-sveltekit-preload-data="hover"
-	>
-		<div class="mx-auto max-w-2xl bg-white px-3 py-3 text-black dark:bg-deeppurple dark:text-white">
-			{#if $page.data.session}
-				{#if $page.data.session.user?.image}
-					<div class="flex flex-row items-center justify-between">
-						<div class="flex flex-row items-center space-x-3">
-							<img
-								src={$page.data.session.user.image}
-								class="h-9 w-9 rounded-full object-cover"
-								alt={$page.data.session.user.name}
-							/>
+<main
+	class="mx-auto bg-white px-3 font-satoshi text-black dark:bg-deeppurple dark:text-white"
+	data-sveltekit-preload-data="hover"
+>
+	<div class="mx-auto max-w-2xl bg-white px-3 py-3 text-black dark:bg-deeppurple dark:text-white">
+		{#if $page.data.session}
+			{#if $page.data.session.user?.image}
+				<div class="flex flex-row items-center justify-between">
+					<div class="flex flex-row items-center space-x-3">
+						<img
+							src={$page.data.session.user.image}
+							class="h-9 w-9 rounded-full object-cover"
+							alt={$page.data.session.user.name}
+						/>
 
-							<span>
-								<small>Signed in as</small><br />
-								<strong>{$page.data.session.user?.name ?? 'User'}</strong>
-							</span>
-						</div>
-
-						<button on:click={() => signOut()} class="button">Sign out</button>
-					</div>
-				{/if}
-			{:else}
-				<button on:click={() => ($showLoginModal = true)}> Sign in with GitHub </button>
-			{/if}
-
-			{#if data.city !== 'unknown' || data.country !== 'unknown'}
-				<p id="lastvisit" class=" text-gray-500 dark:text-gray-400">
-					Last visit is from: {data.city}, {data.country}
-				</p>
-			{/if}
-
-			<Header />
-
-			{#if $showLoginModal}
-				<Modal showModal={$showLoginModal}>
-					<h3 slot="header">Welcome to Spikey's Portfolio</h3>
-					<div slot="content" class="flex flex-col space-y-3">
-						<p>
-							In order to interact with the portfolio like asking questions, you need to sign in
-							with GitHub.
-						</p>
+						<span>
+							<small>Signed in as</small><br />
+							<strong>{$page.data.session.user?.name ?? 'User'}</strong>
+						</span>
 					</div>
 
-					<div slot="cta" class="flex w-full flex-col space-y-3">
-						<button
-							on:click={() => signIn()}
-							class="flex w-full items-center justify-center rounded-md bg-black px-2 py-2 font-medium text-white hover:bg-gray-900"
-							>Sign in with GitHub</button
-						>
-
-						<button
-							class="flex w-full items-center justify-center rounded-md border border-gray-300 bg-gray-100 px-2 py-2 text-black hover:bg-gray-200"
-							on:click={() => ($showLoginModal = false)}>Close modal</button
-						>
-					</div>
-				</Modal>
+					<button on:click={() => signOut()} class="button">Sign out</button>
+				</div>
 			{/if}
+		{:else}
+			<button on:click={() => ($showLoginModal = true)}> Sign in with GitHub </button>
+		{/if}
 
-			<slot />
-		</div>
-	</main>
-{/key}
+		{#if data.city !== 'unknown' || data.country !== 'unknown'}
+			<p id="lastvisit" class=" text-gray-500 dark:text-gray-400">
+				Last visit is from: {data.city}, {data.country}
+			</p>
+		{/if}
+
+		<Header />
+
+		{#if $showLoginModal}
+			<Modal showModal={$showLoginModal}>
+				<h3 slot="header">Welcome to Spikey's Portfolio</h3>
+				<div slot="content" class="flex flex-col space-y-3">
+					<p>
+						In order to interact with the portfolio like asking questions, you need to sign in with
+						GitHub.
+					</p>
+				</div>
+
+				<div slot="cta" class="flex w-full flex-col space-y-3">
+					<button
+						on:click={() => signIn()}
+						class="flex w-full items-center justify-center rounded-md bg-black px-2 py-2 font-medium text-white hover:bg-gray-900"
+						>Sign in with GitHub</button
+					>
+
+					<button
+						class="flex w-full items-center justify-center rounded-md border border-gray-300 bg-gray-100 px-2 py-2 text-black hover:bg-gray-200"
+						on:click={() => ($showLoginModal = false)}>Close modal</button
+					>
+				</div>
+			</Modal>
+		{/if}
+
+		<slot />
+	</div>
+</main>
