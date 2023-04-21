3b3b261eacb733b1be318b53f77cc10d03e24809
Spikey Sanju <spikeysanju98@gmail.com>
2023-04-21 09:33:22 +0530

    feat: add links to social media

---
+++ src/lib/components/markdown/Link.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let text = '';
+	export let href = '';
+	export let raw = '';
+</script>
+
+<a {href} class="font-medium text-blue-500 underline hover:cursor-pointer dark:text-blue-400">
+	{text}
+</a>
